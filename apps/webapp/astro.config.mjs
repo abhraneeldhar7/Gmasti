f7f9caf10854f613ba8bb3c8e12a35cb3c7f2462
Abhraneel <abhraneeldhar7@gmail.com>
2026-05-10 14:12:56 +0530

    vercel adapter

--- apps/webapp/astro.config.mjs
+++ apps/webapp/astro.config.mjs
@@ -2,16 +2,22 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: ["yearlong-jon-patrilineal.ngrok-free.dev"],
     },
   },
+
   server: {
     host: true,
     port: 3000,
   },
+
+  adapter: vercel(),
 });
